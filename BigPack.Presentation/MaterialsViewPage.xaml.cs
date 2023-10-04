9d029becaa12a8db76f9e517ef3200f61162d4f0
Александр Сантуров <santurovs866@gmail.com>
2023-10-05 02:18:00 +0500

    Жопа

--- BigPack.Presentation/MaterialsViewPage.xaml.cs
+++ BigPack.Presentation/MaterialsViewPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -25,6 +26,7 @@ namespace BigPack.Presentation
     public partial class MaterialsViewPage : Page
     {
         private readonly BigPackDbContext _dbContext;
+        private List<MaterialViewModel> materials;
         public MaterialsViewPage()
         {
             _dbContext = new BigPackDbContext();
@@ -37,13 +39,29 @@ namespace BigPack.Presentation
             var materials = _dbContext.Materials
                 .ProjectTo<MaterialViewModel>(mapper.ConfigurationProvider)
                 .ToList();
-
             MaterialsListView.ItemsSource = materials;
+
+            Update();
+        }
+
+        private void Update()
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            mc.AddProfile(new MaterialViewModelProfiler()));
+            var mapper = mapperConfig.CreateMapper();
+
+            var currentMaterials = _dbContext.Materials
+                .ProjectTo<MaterialViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            currentMaterials = currentMaterials.Where(material =>
+                material.MaterialName.ToLower().Contains(TextBoxSearch.Text.Trim().ToLower())).ToList();
+            MaterialsListView.ItemsSource = currentMaterials;
         }
 
         private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Update();
         }
 
 
